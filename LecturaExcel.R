0a15c87119f75dc670a989fc2a9823330e010f8e
Víctor Caquilpan <victor.caquilpan@gmail.com>
2021-10-16 12:31:55 -0300

    Script con purrr y furrr

--- LecturaExcel.R
+++ LecturaExcel.R
@@ -41,8 +41,26 @@ Hojas <- map(Archivos, ~ excel_sheets(.x))
 
 # Creamos una función para leer las hojas
 LecturaExcel <- function(Archivos,Hojas) {
-  Datos <- map_df(Hojas, ~ read_excel(path = Archivos,sheet = .x),Archivos)}
+   map(Hojas, ~ read_excel(path = Archivos,sheet = .x),Archivos)}
 
 # Extraemos la informacion de todas las hojas
-Datos <- map2_df(Archivos,Hojas, LecturaExcel)
+Datos <- map2_dfr(Archivos,Hojas, LecturaExcel)
+
+# Prueba utilizando furrr ----
+
+# Leemos los archivos disponibles
+Archivos <- list.files("datos",full.names = TRUE)
+
+# Declaramos que correremos nuestro proceso en múltiples sesiones de R
+future::plan(multisession)
+
+# Primero tenemos que identificar las hojas por cada archivo
+Hojas <- future_map(Archivos, ~ excel_sheets(.x), .options = furrr_options(seed = T))
+
+# Creamos una función para leer las hojas
+LecturaExcel <- function(Archivos,Hojas) {
+  future_map(Hojas, ~ read_excel(path = Archivos,sheet = .x),Archivos, .options = furrr_options(seed = T))}
+
+# Extraemos la informacion de todas las hojas
+Datos <- future_map2_dfr(Archivos,Hojas, LecturaExcel, .options = furrr_options(seed = T))
 

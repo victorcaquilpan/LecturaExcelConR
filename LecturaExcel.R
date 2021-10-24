1e2bcb6bca32946810301360e426e957e4c32215
Víctor Caquilpan <victor.caquilpan@gmail.com>
2021-10-24 11:39:58 -0300

    inclusion de benchmark

--- LecturaExcel.R
+++ LecturaExcel.R
@@ -61,3 +61,53 @@ LecturaExcel <- function(Archivos,Hojas) {
 # Extraemos la informacion de todas las hojas
 Datos <- future_map2_dfr(Archivos,Hojas, LecturaExcel, .options = furrr_options(seed = T))
 
+# Finalmente podemos realizar una comparación con microbenchmark
+library(microbenchmark)
+
+# Analisis
+microbenchmark(ciclofor = {
+  # Operacion con for
+  # Creamos una lista vacia para recojer los datos
+  Datos <- data.frame()
+  
+  # Creamos un ciclo para recorrer los archivos
+  for (archivo in seq_along(Archivos)) {
+    
+    # Vemos las hojas disponibles en cada archivo
+    Hojas <- excel_sheets(Archivos[archivo])
+    
+    # Creamos un ciclo para recorrer hojas
+    for (hoja in seq_along(Hojas)) {
+      
+      # Alojamos los datos en nuestra lista
+      Documento <- read_excel(path = Archivos[archivo],sheet = Hojas[hoja])
+      
+      # Apendizamos nuestros datos al objeto datos
+      Datos <- rbind(Datos,Documento)
+    }
+  }
+},  purrr = {
+  # Operacion con purrr
+  # Primero tenemos que identificar las hojas por cada archivo
+  Hojas <- map(Archivos, ~ excel_sheets(.x))
+  
+  # Creamos una función para leer las hojas
+  LecturaExcel <- function(Archivos,Hojas) {
+    map(Hojas, ~ read_excel(path = Archivos,sheet = .x),Archivos)}
+  
+  # Extraemos la informacion de todas las hojas
+  Datos <- map2_dfr(Archivos,Hojas, LecturaExcel)
+
+},  furrr = {
+  # Operacion con furrr
+  # Primero tenemos que identificar las hojas por cada archivo
+  Hojas <- future_map(Archivos, ~ excel_sheets(.x), .options = furrr_options(seed = T))
+  
+  # Creamos una función para leer las hojas
+  LecturaExcel <- function(Archivos,Hojas) {
+    future_map(Hojas, ~ read_excel(path = Archivos,sheet = .x),Archivos, .options = furrr_options(seed = T))}
+  
+  # Extraemos la informacion de todas las hojas
+  Datos <- future_map2_dfr(Archivos,Hojas, LecturaExcel, .options = furrr_options(seed = T))
+  
+},times = 10)
